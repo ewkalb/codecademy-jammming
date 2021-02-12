50f558c932a41e881803bad0fa726f2c267e2c48
depthstar <atempovivo@gmail.com>
2021-02-11 23:46:14 -0600

    section 3 complete

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,21 +2,32 @@ import './App.css';
 import SearchBar from '../SearchBar/SearchBar'
 import SearchResults from '../SearchResults/SearchResults'
 import Playlist from '../Playlist/Playlist'
+import react from 'react';
 
 
-function App() {
+class App extends react.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResults:[
+      {name: 'name1', artist: 'artists1', album: 'album1', id: 1},
+      {name: 'name2', artist: 'artists2', album: 'album2', id: 2},
+      {name: 'name3', artist: 'artists3', album: 'album3', id: 3},
+    ]}
+  }
+  render() {
   return (
     <div>
       <h1>Ja<span className="highlight">mmm</span>ing</h1>
-        <div className="App">
-          <SearchBar />
+      <div className="App">
+        <SearchBar />
         <div className="App-playlist">
-          <SearchResults />
+          <SearchResults searchResults={this.state.searchResults} />
           <Playlist />
+          </div>
         </div>
-      </div>
     </div>
-  );
+  );}
 }
 
 export default App;
